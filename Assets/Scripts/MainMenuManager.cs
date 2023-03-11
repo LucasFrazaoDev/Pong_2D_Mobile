f5fea1c81637f5da4558bff1a98073688202b4b9
Lucas Frazão <lucas_devfrazao@outlook.com>
2023-03-10 21:18:18 -0300

    Game settings logic started

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,20 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] GameObject panelMainMenu;
-    [SerializeField] GameObject panelSetGame;
+    private int _difficultySelected;
+    private int _highScoreSelected;
+
+    [Header("Panels")]
+    [SerializeField] GameObject _panelMainMenu;
+    [SerializeField] GameObject _panelSetGame;
+
+    [Header("Toggle Groups")]
+    [SerializeField] ToggleGroup _difficultyToggleGroup;
+    [SerializeField] ToggleGroup _highScoreToggleGroup;
+
+    private void Start()
+    {
+        _difficultySelected = 0;
+        _highScoreSelected = 0;
+    }
 
     #region MainMenu
     public void NewGame()
     {
-        panelMainMenu.SetActive(false);
-        panelSetGame.SetActive(true);
+        _panelMainMenu.SetActive(false);
+        _panelSetGame.SetActive(true);
     }
 
     public void QuitGame()
@@ -27,6 +42,7 @@ public class MainMenuManager : MonoBehaviour
     }
     #endregion
 
+    #region GameSet
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -34,7 +50,36 @@ public class MainMenuManager : MonoBehaviour
 
     public void SetGameBackToMainMenu()
     {
-        panelMainMenu.SetActive(true);
-        panelSetGame.SetActive(false);
+        _panelMainMenu.SetActive(true);
+        _panelSetGame.SetActive(false);
     }
+
+    public void OnDifficultyToggleChanged()
+    {
+        _difficultySelected = GetSelectedToggleIndex(_difficultyToggleGroup);
+        Debug.Log("Selected toggle index: " + _difficultySelected);
+    }
+
+    public void OnHighScoreToggleChanged()
+    {
+        _highScoreSelected = GetSelectedToggleIndex(_highScoreToggleGroup);
+        Debug.Log("Toggle index selecionado: " + _highScoreSelected);
+    }
+
+    private int GetSelectedToggleIndex(ToggleGroup toggleGroup)
+    {
+        Toggle[] toggles = toggleGroup.GetComponentsInChildren<Toggle>();
+
+        for (int i = 0; i < toggles.Length; i++)
+        {
+            if (toggles[i].isOn)
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    #endregion
 }
